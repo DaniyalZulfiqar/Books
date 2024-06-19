3c6b43b89913347eaa6ecb4c7b1233bf067c9cda
Daniyal Zulfiqar <daniyal.uw@gmail.com>
2024-06-19 17:22:50 +0100

    Update useContext useEffect

---
+++ src/components/BookCreate.js
@@ -0,0 +1,28 @@
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
+
+function BookCreate ({onCreate}) {
+    const [title, setTitle] = useState('');
+
+    const {createBook} = useContext(BooksContext);
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleSubmit =  (event) => {
+        event.preventDefault();
+        createBook(title);
+        setTitle('');
+    }
+    return <div className="book-create">
+        <form onSubmit={handleSubmit}>
+            <h3>Add a Book</h3>
+            <label> Title</label>
+            <input className='input' value={title} onChange={handleChange}/>
+            <button className="button">Create!</button>
+        </form>
+    </div>;
+}
+
+export default BookCreate;
